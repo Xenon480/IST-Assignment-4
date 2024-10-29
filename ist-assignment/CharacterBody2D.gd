05bb3e812da61dcf83b35f208e6e40fed341bb50
Ediblethumb2 <145306928+Ediblethumb2@users.noreply.github.com>
2024-10-29 18:55:56 +1100

    REALCOMMITREAAAAAAAAAAAAL
    REAAAAAL

--- ist-assignment/CharacterBody2D.gd
+++ ist-assignment/CharacterBody2D.gd
@@ -3,10 +3,10 @@ var StartPos = global_position
 @export  var Shapecast: ShapeCast2D
 @export var Area2d: Area2D
 @onready var Projectile = get_node("CharacterBody2D")
-@export var AnimatedSprite: AnimatedSprite2D 
 var Entered = false
 var PlayerLoc = Vector2.ZERO
 var PlayerBody2D = CharacterBody2D
+@export var AnimatedSprite: AnimatedSprite2D 
 var scene = load("res://Projectile.tscn")
 func _physics_process(delta):
 	
@@ -31,6 +31,11 @@ func _physics_process(delta):
 
 func _on_timer_timeout():
 	if Shapecast.get_collider(0):
+		AnimatedSprite.play("Archer-Shoot")
 		var dupe = scene.instantiate()
 		add_child(dupe)
 		dupe.global_position = global_position
+		dupe.set_meta("Direction",(Shapecast.get_collider(0).get_node("CollisionShape2D").global_position - get_node("CollisionShape2D").global_position).normalized())
+		
+	
+		
