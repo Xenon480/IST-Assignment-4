4e96a7fad839c56c43271db0b5e2e349d3b4d563
Ediblethumb2 <145306928+Ediblethumb2@users.noreply.github.com>
2024-10-31 21:43:07 +1100

    fff
    ffff

--- ist-assignment/projectile.gd
+++ ist-assignment/projectile.gd
@@ -7,14 +7,14 @@ const JUMP_VELOCITY = -400.0
 
 	
 func _physics_process(delta: float) -> void:
-	
+	#Settign the velocity of the arrow by taking the direction that was fed into by the archer entity 
 	velocity = get_meta("Direction") * 500
 	velocity.y += 10
 	move_and_slide()
 	
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
-	
+	#Checking if the arrrow entered any nodes if its the tilemap then destory it if its the player then damage and then destroy
 	if body.name == "TileMap":
 		queue_free()
 	if body.name == "Player":
